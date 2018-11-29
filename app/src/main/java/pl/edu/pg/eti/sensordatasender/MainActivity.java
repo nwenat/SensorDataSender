3d9a76e4f4d5bd53bed96c4eebe0389b25e31d21
Natalia Dorozynska <natala.dorozynska@gmail.com>
2018-11-29 17:38:48 +0100

    add cound of call

--- app/src/main/java/pl/edu/pg/eti/sensordatasender/MainActivity.java
+++ app/src/main/java/pl/edu/pg/eti/sensordatasender/MainActivity.java
@@ -1,16 +1,24 @@
 package pl.edu.pg.eti.sensordatasender;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
     SensorDataSender sensorDataSender = new SensorDataSender();
+    private String path = Environment.getExternalStorageDirectory().toString() + "/DigitalZombieLab/KenisToys";
+    private final int MEMORY_ACCESS = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_CALL_LOG)) {}
+        else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CALL_LOG}, MEMORY_ACCESS);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MEMORY_ACCESS:
+                if ((grantResults.length > 0)&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {}
+                else {
+                    Toast.makeText(getApplicationContext(), "Jeśli nie zostanie wyrażona zgoda na dostęp do pamięci, nie będzie możliwości zapisania pliku", Toast.LENGTH_LONG).show();
+                }
+        }
     }
 
     public void click(View view) {
@@ -28,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 String serverAddress = editText.getText().toString();
                 Log.d("activityyy", editText.getText().toString());
 
-                sensorDataSender.sendData(serverAddress);
+                sensorDataSender.sendData(getApplicationContext(), serverAddress);
 
                 break;
         }
--- app/src/main/java/pl/edu/pg/eti/sensordatasender/SensorDataSender.java
+++ app/src/main/java/pl/edu/pg/eti/sensordatasender/SensorDataSender.java
@@ -1,9 +1,46 @@
 package pl.edu.pg.eti.sensordatasender;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.CallLog;
+import android.util.Log;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SensorDataSender {
 
-    public void sendData(String serverAddress) {
 
+    public void sendData(Context context, String serverAddress) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+        String dateTime = simpleDateFormat.format(new Date());
+
+        Log.d("activityyy", dateTime);
+
+        getCallsData(context);
+        getSMSDate();
+    }
+
+    public void getCallsData(Context context) {
+
+        String[] projection = { CallLog.Calls.CACHED_NAME, CallLog.Calls.CACHED_NUMBER_LABEL, CallLog.Calls.TYPE };
+        String where;
+
+        where = CallLog.Calls.TYPE+"="+CallLog.Calls.INCOMING_TYPE;
+        Cursor incomingType = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, projection, where, null, null);
+        Log.d("INCOMING CALL", ""+incomingType.getCount());
+
+        where = CallLog.Calls.TYPE+"="+CallLog.Calls.OUTGOING_TYPE;
+        Cursor outGoingType = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, projection, where, null, null);
+        Log.d("OUT GOING CALL", ""+outGoingType.getCount());
+
+        where = CallLog.Calls.TYPE+"="+CallLog.Calls.MISSED_TYPE;
+        Cursor missedType = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, projection, where, null, null);
+        Log.d("MISSED CALL", ""+missedType.getCount());
+
+        int incoming = incomingType.getCount();
+        int outGoing = outGoingType.getCount();
+        int missed = missedType.getCount();
     }
 
     private void getSMSDate() {}
